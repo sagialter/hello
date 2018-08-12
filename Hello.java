8ee7cf7948530419f655f06734c8a3c317c8acee
sagi <sagidevops8.gmail.com>
2018-08-12 18:36:15 +0300

    hello

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by alter on 12/08/2018.
+ */
+public class Hello {
+    public static void main(String[] args){
+        for (int i=1; i <=10 ; i++){
+            System.out.println("Hello World ..." + i);
+        }
+    }
+}
